917c82cbcd32a90f483c32e5a16112e4244c380e
augustinde <augustin.1201@outlook.fr>
2023-04-29 11:15:46 +0200

    add firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'avian-foundry-384513',
+    appId: '1:177785194243:web:1cd0f307a16fdd27454b36',
+    storageBucket: 'avian-foundry-384513.appspot.com',
+    apiKey: 'AIzaSyA3RN5OmE4sd7lPevK1e_UTc6hWjF1nj38',
+    authDomain: 'avian-foundry-384513.firebaseapp.com',
+    messagingSenderId: '177785194243',
+    measurementId: 'G-GPYM1Q6LW4',
+  },}
