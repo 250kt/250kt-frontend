3d1575da06b21eb1f3e8209b96ada9440b261920
augustinde <augustin.1201@outlook.fr>
2023-08-31 21:44:00 +0200

    component: vertical navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,18 +3,36 @@ import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "../app-routing.module";
 import {FooterComponent} from "./components/footer/footer.component";
 import {HeaderComponent} from "./components/header/header.component";
+import {SideNavComponent} from "./components/sidenav/sidenav.component";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatButtonModule} from "@angular/material/button";
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SideNavComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    NgOptimizedImage
   ],
   providers: [],
-  exports: [HeaderComponent, FooterComponent]
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SideNavComponent,
+  ]
 })
 export class CoreModule {
 }
