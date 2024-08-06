4dbb8de7593a32aaf09c484b78ca0bb0a07bbcb3
Augustin D <42963508+augustinde@users.noreply.github.com>
2024-08-06 23:34:24 +0200

    feat: allow to load flight + refactoring some code (#29)

--- src/app/service/flight.service.ts
+++ src/app/service/flight.service.ts
@@ -2,6 +2,9 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Flight} from "../shared/model/flight";
+import {Observable} from "rxjs";
+import {Aircraft} from "../shared/model/aircraft";
+import {Airfield} from "../shared/model/airfield";
 
 @Injectable({
     providedIn: 'root'
@@ -14,20 +17,35 @@ export class FlightService {
 
     private url = environment.backendUrl + '/flight';
 
-    createFlight(flight: Flight){
-        return this.http.post(this.url, flight);
+    createFlight(): Observable<Flight>{
+        return this.http.post(this.url,{});
     }
 
-    getCurrentUserFlight(){
-        return this.http.get<Flight>(this.url);
+    getCurrentUserFlight(): Observable<Flight>{
+        return this.http.get<Flight>(`${this.url}/current`);
     }
 
-    updateFlight(flight: Flight){
-        return this.http.put(`${this.url}`, flight);
+    updateFlight(flight: Flight) : Observable<Flight>{
+        return this.http.put<Flight>(`${this.url}`, flight);
     }
 
-    archiveFlight(flight: Flight){
-        return this.http.put(`${this.url}/archive`, flight);
+    getUserFlights(){
+        return this.http.get<Flight[]>(this.url);
     }
 
+    changeCurrentFlight(flight: Flight): Observable<Flight>{
+        return this.http.put(`${this.url}/current`, flight);
+    }
+
+    changeAircraft(aircraft: Aircraft): Observable<Flight>{
+        return this.http.put(`${this.url}/aircraft`, aircraft);
+    }
+
+    changeDepartureAirfield(airfield: Airfield): Observable<Flight>{
+        return this.http.put(`${this.url}/airfield/departure`, airfield);
+    }
+
+    changeArrivalAirfield(airfield: Airfield): Observable<Flight>{
+        return this.http.put(`${this.url}/airfield/arrival`, airfield);
+    }
 }
