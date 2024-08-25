f0df8d4ce256845bd0f77f24bb5b4126b1676325
Augustin D <42963508+augustinde@users.noreply.github.com>
2024-08-25 23:06:29 +0200

    feat: add airfield weather (#38)

---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {AirfieldWeather} from "../shared/model/weather";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WeatherService {
+
+    constructor(
+        private readonly http: HttpClient,
+    ) {}
+
+    private url = environment.backendUrl + '/weather';
+
+    getAirfieldWeather(airfieldCode: string): Observable<AirfieldWeather> {
+        return this.http.get<AirfieldWeather>(this.url + '/' + airfieldCode);
+    }
+
+}
