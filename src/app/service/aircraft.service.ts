8665b5e90954a98c479c8019ba28402fbc798c4f
augustinde <augustin.1201@outlook.fr>
2024-11-18 21:45:58 +0100

    update to angular 18

--- src/app/service/aircraft.service.ts
+++ src/app/service/aircraft.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import {Aircraft} from "../shared/model/aircraft";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
